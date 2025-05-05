ea4115cfe93fa1ff89fcdb2619379942b5fe2116
Yuldoshova <ozoda8171@gmail.com>
2025-05-05 10:21:12 +0500

    feat: added app configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService)
+
+   // Get configuration values with defaults
+  const port = configService.get<number>('APP_PORT', 3000)
+  const host = configService.get<string>('APP_HOST', 'localhost')
+  const environment = configService.get<string>('NODE_ENV', 'development')
+
+  await app.listen(port, host,
+    () => console.log(`Application is running in ${environment} mode on port ${port}`)
+  );
+
 }
+
 bootstrap();
